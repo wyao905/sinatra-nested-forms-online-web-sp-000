529a02a3478ab3ac13300f432f66445d01102e45
Warren Yao <warrenyca905@gmail.com>
2019-09-18 01:47:32 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+  
+  @@all = []
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all = []
+  end
 end
